e1b779eaeb5fa73c1317b5e06336f6e3e74ec016
Yosuke ONOUE <onoue@likr-lab.com>
2014-10-19 17:09:28 +0900

    Add draw button to dataset

--- src/directives/datset-control.js
+++ src/directives/datset-control.js
@@ -1,29 +1,68 @@
-angular.module('opendap-viewer')
-  .controller('DatasetController', class ControlController {
+function queryUrl(data) {
+  var range = data.query.map(q => `[${q}]`).join('');
+  return `${data.url}.dods?${data.name}${range}`;
+}
 
+
+function coordinates(data, dataset) {
+  var result = new Set();
+  data.array.dimensions.forEach(dimension => {
+    var axis = dataset[dimension].attributes.axis;
+    if (['X', 'Y', 'Z'].indexOf(axis) > -1) {
+      result.add(axis);
+    }
+  });
+  return result;
+}
+
+
+angular.module('opendap-viewer')
+  .controller('DatasetController', class {
     constructor($scope) {
       this.$scope = $scope;
-      this.dataset = [];
+      this.grid = [];
       this.url = 'http://localhost/dias/thredds/dodsC/DIAS/MOVE-RA2014';
     }
 
     loadDataset() {
-      jqdap.loadDataset(this.url)
+      var url = this.url;
+      jqdap.loadDataset(url)
         .then(dataset => {
-          var key, value;
+          var key, data, axes;
           for (key in dataset) {
-            value = dataset[key];
-            if (value.type && value.type == 'Grid') {
-              this.dataset.push(value);
+            data = dataset[key];
+            if (data.type) {
+              data.url = url;
+              if (data.type === 'Grid') {
+                this.grid.push(data);
+                data.query = data.array.shape.map((shape, i) => {
+                  return `0:${shape - 1}`;
+                });
+                axes = coordinates(data, dataset);
+                data.draw = {
+                  contour: axes.size >= 2,
+                  isosurface: axes.size === 3,
+                  pbr: axes.size === 3
+                };
+              }
             }
           }
           this.$scope.$apply();
         });
     }
+
+    draw(data) {
+      var url = queryUrl(data);
+      console.log(url, data);
+      jqdap.loadData(url)
+        .then(data => {
+          console.log(data);
+        });
+    }
   })
   .directive('datasetControl', () => {
     return {
-      controller: 'DatasetController as ctl',
+      controller: 'DatasetController as datasetCtl',
       restrict: 'E',
       templateUrl: 'partials/directives/dataset-control.html',
     };
