a6469300544391a1be56f3865afda9f047d7b4c6
Yosuke ONOUE <onoue@likr-lab.com>
2014-10-22 12:18:06 +0900

    Fix basic auth access error

--- src/services/jqdap.js
+++ src/services/jqdap.js
@@ -1,4 +1,15 @@
 angular.module('opendap-viewer')
-  .factory('jqdap', ($window) => {
-    return $window.jqdap;
+  .factory('jqdap', ($window, $q) => {
+    return {
+      loadDataset: (url) => {
+        return $q.when($window.jqdap.loadDataset(url, {
+          withCredentials: true,
+        }));
+      },
+      loadData: (url) => {
+        return $q.when($window.jqdap.loadData(url, {
+          withCredentials: true,
+        }));
+      }
+    };
   });
