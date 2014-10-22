c5f4f235012fc43a0d33ce7bf9b2099c285b9eab
Yosuke ONOUE <onoue@likr-lab.com>
2014-10-22 23:07:56 +0900

    Allow basic auth with browser dialog

--- src/services/jqdap.js
+++ src/services/jqdap.js
@@ -2,9 +2,11 @@ angular.module('opendap-viewer')
   .factory('jqdap', ($window, $q) => {
     return {
       loadDataset: (url, options) => {
+        options.withCredentials = true;
         return $q.when($window.jqdap.loadDataset(url, options));
       },
       loadData: (url, options) => {
+        options.withCredentials = true;
         return $q.when($window.jqdap.loadData(url, options));
       }
     };
